c2b61cf4eca589d864b0b3446a25bee067f714d4
chetandp <chetan15495@gmail.com>
2022-08-26 02:43:05 +0530

    middleware assignment

--- src/index.js
+++ src/index.js
@@ -3,23 +3,34 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment= require('moment')
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://chetan:jA86skOO0CAvH5ou@cluster0.srfgso1.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
+app.use (
+    function (req, res, next) {
+        const date=moment().format('DD-MM-YYYY, HH:mm:ss');
+        const ipAddress= req.ip;
+        const r=req.originalUrl
+        console.log (date," , ",ipAddress," , ",r);
+        next();
+  }
+  );
+//app.use (
+  // function (req, res, next) {
+   //     console.log ("inside GLOBAL MW");
+   //   next();
+// }
+//);
   
 
 app.use('/', route);
